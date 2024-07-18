583d2d485fca41a64ed37c400406c7c4b2abdf05
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-07-18 16:03:17 +0200

    dev: implementation of other validations

--- main-project/src/main/java/it/davide/course/mainproject/controller/CustomerController.java
+++ main-project/src/main/java/it/davide/course/mainproject/controller/CustomerController.java
@@ -3,10 +3,13 @@ package it.davide.course.mainproject.controller;
 import it.davide.course.mainproject.model.Customer;
 import it.davide.course.mainproject.model.Student;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -27,9 +30,19 @@ public class CustomerController {
             Customer customer,
             BindingResult bindingResult) {
 
+        System.out.println("Customer last name: |" + customer.getLastName() + "|");
+        System.out.println("Binding result: " + bindingResult.toString());
+
         if(bindingResult.hasErrors()) {
             return "customer-form";
         }
         return "customer-confirmation";
+    }
+
+    //To remove leading and trailing whitespace
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        binder.registerCustomEditor(String.class, stringTrimmerEditor);
     }
 }
--- main-project/src/main/java/it/davide/course/mainproject/model/Customer.java
+++ main-project/src/main/java/it/davide/course/mainproject/model/Customer.java
@@ -1,7 +1,6 @@
 package it.davide.course.mainproject.model;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,4 +15,12 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName = "";
+
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "must be greater than or equal to 0")
+    @Max(value = 10, message = "must be less than or equal to 10")
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
 }
