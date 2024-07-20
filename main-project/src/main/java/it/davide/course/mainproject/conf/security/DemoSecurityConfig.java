ad57d515af2165c9f6703cd49bcac4069faf98bb
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-07-20 15:10:11 +0200

    dev: implement logout

--- main-project/src/main/java/it/davide/course/mainproject/conf/security/DemoSecurityConfig.java
+++ main-project/src/main/java/it/davide/course/mainproject/conf/security/DemoSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -64,6 +65,7 @@ public class DemoSecurityConfig {
                         .loginPage("/login")
                         .loginProcessingUrl("/authenticate")
                         .permitAll()
+        ).logout(LogoutConfigurer::permitAll
         );
 
         return http.build();
