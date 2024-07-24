3c6937d5503fa11c6eafca7ddd51d3cbee896667
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-07-24 14:42:49 +0200

    conf: add new project for AOP

---
+++ aop-demo/src/main/java/it/davide/course/aop/AopDemoApplication.java
@@ -0,0 +1,22 @@
+package it.davide.course.aop;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopDemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World");
+		};
+	}
+
+}
---
+++ aop-demo/src/test/java/it/davide/course/aop/AopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.davide.course.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
