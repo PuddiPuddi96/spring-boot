79eabf8996809b744edc8786bc90fbdff3038f7c
Davide Benedetto Strianese <d.strianese4@studenti.unisa.it>
2024-08-24 19:02:50 +0200

    conf: set-up backend project

---
+++ lascaux-challenge-be/Backend/challenge-backend/src/main/java/it/davide/lascaux/challenge/backend/ChallengeBackendApplication.java
@@ -0,0 +1,13 @@
+package it.davide.lascaux.challenge.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBackendApplication.class, args);
+	}
+
+}
---
+++ lascaux-challenge-be/Backend/challenge-backend/src/test/java/it/davide/lascaux/challenge/backend/ChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.davide.lascaux.challenge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
